56d53e1687d901785136992a7be6e9cb5cfab0cf
DREXYOP <91460817+DREXYOP@users.noreply.github.com>
2023-03-19 17:24:40 +0000

    better sfw

--- src/main/sfw.js
+++ src/main/sfw.js
@@ -3,99 +3,99 @@ const axios = require('axios');
 
 class Sfw {
   constructor() {
-    this.baseUrl = "https://purrbot.site/api"
+    this.baseUrl = "https://nekos.life/api/v2"
   }
 
-  async blush() {
-    const { data } = await axios.get(`${this.baseUrl}/img/sfw/blush/gif`);
+  async waifu() {
+    const { data } = await axios.get(`${this.baseUrl}/img/waifu`);
     const message = await data.link;
     return message;
   }
 
-  async cry() {
-    const { data } = await axios.get(`${this.baseUrl}/img/sfw/cry/gif`);
+  async spoiler() {
+    const { data } = await axios.get(`${this.baseUrl}/img/spoiler`);
     const message = await data.link;
     return message;
   }
 
   async cuddle() {
-    const { data } = await axios.get(`${this.baseUrl}/img/sfw/cuddle/gif`);
+    const { data } = await axios.get(`${this.baseUrl}/img/cuddle`);
     const message = await data.link;
     return message;
   }
-  async dance() {
-    const { data } = await axios.get(`${this.baseUrl}/img/sfw/dance/gif`);
+  async owoify() {
+    const { data } = await axios.get(`${this.baseUrl}/img/owoify`);
     const message = await data.link;
     return message;
   }
 
   async hug() {
-    const { data } = await axios.get(`${this.baseUrl}/img/sfw/hug/gif`);
+    const { data } = await axios.get(`${this.baseUrl}/img/hug`);
     const message = await data.link;
     return message;
   }
 
   async kiss() {
-    const { data } = await axios.get(`${this.baseUrl}/img/sfw/kiss/gif`);
+    const { data } = await axios.get(`${this.baseUrl}/img/kiss`);
     const message = await data.link;
     return message;
   }
 
   async neko() {
-    const { data } = await axios.get(`${this.baseUrl}/img/sfw/neko/gif`);
+    const { data } = await axios.get(`${this.baseUrl}/img/ngif`);
     const message = await data.link;
     return message;
   }
 
   async slap() {
-    const { data } = await axios.get(`${this.baseUrl}/img/sfw/slap/gif`);
+    const { data } = await axios.get(`${this.baseUrl}/img/slap`);
     const message = await data.link;
     return message;
   }
 
-  async tail() {
-    const { data } = await axios.get(`${this.baseUrl}/img/sfw/tail/gif`);
+  async foxgirl() {
+    const { data } = await axios.get(`${this.baseUrl}/img/fox_girl`);
     const message = await data.link;
     return message;
   }
 
-  async smile() {
-    const { data } = await axios.get(`${this.baseUrl}/img/sfw/smile/gif`);
+  async meow() {
+    const { data } = await axios.get(`${this.baseUrl}/img/meow`);
     const message = await data.link;
     return message;
   }
 
-  async comfy() {
-    const { data } = await axios.get(`${this.baseUrl}/img/sfw/comfy/gif`);
+  async smug() {
+    const { data } = await axios.get(`${this.baseUrl}/img/smug`);
     const message = await data.link;
     return message;
   }
 
-  async background() {
-    const { data } = await axios.get(`${this.baseUrl}/img/sfw/background/gif`);
+  async lizard() {
+    const { data } = await axios.get(`${this.baseUrl}/img/lizard`);
     const message = await data.link;
     return message;
   }
 
-  async dance() {
-    const { data } = await axios.get(`${this.baseUrl}/img/sfw/dance/gif`);
+  async feed() {
+    const { data } = await axios.get(`${this.baseUrl}/img/feed`);
     const message = await data.link;
     return message;
   }
 
-  async okami() {
-    const { data } = await axios.get(`${this.baseUrl}/img/sfw/okami/gif`);
+  async avatar() {
+    const { data } = await axios.get(`${this.baseUrl}/img/avatar`);
     const message = await data.link;
     return message;
   }
 
   async pat() {
-    const { data } = await axios.get(`${this.baseUrl}/img/sfw/pat/gif`);
+    const { data } = await axios.get(`${this.baseUrl}/img/pat`);
     const message = await data.link;
     return message;
   }
   async poke() {
-    const { data } = await axios.get(`${this.baseUrl}/img/sfw/poke/gif`);
+    const { data } = await axios.get(`${this.baseUrl}/img/poke`);
     const message = await data.link;
     return message;
   }
